7224c40520b9631f81a9c64e116850cc7b4cf314
CallMeSam <112816535+REC-SAM@users.noreply.github.com>
2022-10-08 17:45:08 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package Programs;
+
+import java.util.*;
+
+public class Factorial {
+  
+  // I don't know why it not compliing
+  public int fact ( int n ) {
+    return n*fact(n-1);
+  }
+  
+  public static void main ( String [] args ) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter a number: " );
+    int n = in.nextInt();
+    System.out.println("Answer: " + fact(n));
+  }
+}
